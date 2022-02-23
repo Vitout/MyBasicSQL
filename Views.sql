7dc010fb8f1b96bfb31c8ec21a89d1babc5b8594
Vitout <vitoutkulp@gmail.com>
2022-02-23 14:26:26 +1200

    2 Update
    вычисление количества товаров в каждой категории, используя внутреннее объединение

--- Views.sql
+++ Views.sql
@@ -9,3 +9,13 @@ CREATE VIEV vw_goods_categories AS
     categories_name,
     categories_description
   FROM goods_categories AS gc
+
+CREATE VIEW vw_goods_goods_categories AS
+  SELECT g.goods,
+    g.id_categories,
+    gc.categories_name,
+    COUNT(goods) AS cnt
+  FROM vw_goods AS g
+    INNER JOIN vw_goods_categories AS gc
+      ON gc.id_categories = g.id_categories
+        GROUP BY id_categories
