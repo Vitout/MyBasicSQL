fa7ee33df089f4d3430d534996bd93f9e3f82943
Vitout <vitoutkulp@gmail.com>
2022-02-23 14:18:52 +1200

    Update Views.sql
    Создано представление vw_goods_categories

--- Views.sql
+++ Views.sql
@@ -3,3 +3,9 @@ CREATE VIEW vw_goods AS
     goods,
     id_categories
   FROM goods AS g
+  
+CREATE VIEV vw_goods_categories AS
+  SELECT id_categories,
+    categories_name,
+    categories_description
+  FROM goods_categories AS gc
